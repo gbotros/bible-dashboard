d64bc1fc83e90f03b5c72635ee749aa8d36b8c2f
George <George.B.Habib@gmail.com>
2024-06-25 22:44:49 -0500

    remove test nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/book">Go to About</RouterLink>
-  </nav>
   <main>
     <RouterView />
   </main>
--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -5,11 +5,10 @@
 <template>
   <main>
     <div>
+      <RouterLink to="/">Go to Home</RouterLink>
       Just a normal book page
     </div>
   </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
